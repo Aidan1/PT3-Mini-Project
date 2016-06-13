3328f763b84daf5ceb4f3497392eccde3e0f74f0
Aidan Toh <zavier_mx@live.com.my>
2016-06-14 01:10:03 +0800

    Profile complete

--- MINIPROJECT/Lecturer/LecturerMasterPage.Master.cs
+++ MINIPROJECT/Lecturer/LecturerMasterPage.Master.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,35 @@ namespace MINIPROJECT.Lecturer
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string username = Session["username"].ToString();
 
+            SqlConnection conn = null;
+            string connectionString = getConnection();
+            conn = new SqlConnection();
+            conn.ConnectionString = connectionString;
+
+            SqlCommand cmd = new SqlCommand("GetLecturerProfile", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@username", username));
+            conn.Open();
+            using (SqlDataReader dr = cmd.ExecuteReader())
+            {
+                if (dr.Read())
+                {
+                    lecturer_name.Text = dr["lecturerName"].ToString();
+                    lecturer_name1.Text = dr["lecturerName"].ToString();
+                    matricNo.Text = dr["matricNo"].ToString();
+                    gender.Text = dr["gender"].ToString();
+                }
+                dr.Close();
+            }
+            conn.Close();
+            gender.Visible = false;
+        }
+
+        private string getConnection()
+        {
+            return "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\e-campus.mdf;Integrated Security=True";
         }
     }
 }
